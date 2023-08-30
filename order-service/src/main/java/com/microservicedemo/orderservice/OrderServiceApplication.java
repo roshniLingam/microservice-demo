61b380b32fa156c810db6d0fecdb2ad9dcd75cb4
roshniLingam <lingam_roshni.ghrcecs@raisoni.net>
2023-08-30 15:24:28 +0530

    Added configurations for API Gateway

--- order-service/src/main/java/com/microservicedemo/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservicedemo/orderservice/OrderServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
